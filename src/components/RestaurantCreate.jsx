ce36e15d47060cf539c08239dfb9205e27004a93
saul <sacorvacho@hotmail>
2024-07-25 22:57:52 -0400

    holala

--- src/components/RestaurantCreate.jsx
+++ src/components/RestaurantCreate.jsx
@@ -1,71 +1,50 @@
-import { useState } from "react";
 import api from "../service/api";
+import { useState } from "react";
 
 export function RestaurantCreate() {
-    const [nombre, setNombre] = useState('');
-    const [direccion, setDireccion] = useState('');
-    const [imagenes, setImagenes] = useState(null);
+    const [name, setName] = useState('');
+    const [address, setaddress] = useState('');
+    const [image, setImage] = useState(null);
 
     const handleSubmit = async (evento) => {
         evento.preventDefault();
         console.log("Se previno la acción por defecto");
-        const formData = new FormData();
-        formData.append('nombre', nombre);
-        formData.append('direccion', direccion);
-        formData.append('imagenes', imagenes); // Esto es un archivo de tipo file
 
+        console.log(name, address, image);
+        const restaurante = new FormData();
+        restaurante.append('name', name);
+        restaurante.append('address', address);
+        restaurante.append('image', image);
 
         try {
-            await api.post('/restaurants/', formData, {
+            await api.post('/restaurants/', restaurante, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             });
-            alert('Restaurante agregado correctamente');
+            alert('Restaurant agregado');
         } catch (error) {
-            console.error('Error al crear el restaurante:', error.message);
-            if (error.message.includes('imagenes')) {
-                console.error('Error al cargar la imagen. Asegúrate de que sea un archivo válido.');
-            } else {
-                console.error('Verifica que todos los campos estén correctamente llenados.');
-            }
+            console.log(error.message);
         }
     };
-
-    return (
-        <div className="container mt-4 border p-2">
-            <h2>Create Restaurant</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label className="form-label">Name:</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={nombre}
-                        onChange={(e) => setNombre(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Address:</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        value={direccion}
-                        onChange={(e) => setDireccion(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Image:</label>
-                    <input
-                        type="file"
-                        className="form-control"
-                        onChange={(e) => setImagenes(e.target.files[0])}
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+return(
+    <div className="container mt-4 border p-2">
+    <h2>Create Restaurant</h2>
+    <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+            <label className="form-label">Name:</label>
+            <input type="text" className="form-control" onChange={(e) => setName(e.target.value)} required/>
         </div>
-    );
+        <div className="mb-3">
+            <label className="form-label">Address:</label>
+            <input type="text" className="form-control" onChange={(e) => setaddress(e.target.value)} required/>
+        </div>
+        <div className="mb-3">
+            <label className="form-label">Image:</label>
+            <input type="file" className="form-control" onChange={(e) => setImage(e.target.files[0])}/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+</div>
+)
 }
